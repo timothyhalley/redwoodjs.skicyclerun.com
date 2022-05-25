fafff111acede09019d8904a37bc7420d18af0e2
Tim Halley <timothy.halley@gmail.com>
2022-05-25 15:38:22 -0700

    MAY 25 - Add TailwindCSS and web cleanup

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -8,8 +8,23 @@ const AboutPage = () => {
 
       <main>
         <p>
-          This site was created to demonstrate my mastery of Redwood: Look on my
-          works, ye mighty, and despair!
+          Astrophysicists modeled the solar system’s behavior in two ways.
+          First, they used decades of NASA data to train a neural network. They
+          then used a symbolic regression algorithm to further distill that
+          model into an equation. In these videos — which show true positions as
+          solid objects, and model predictions as wire mesh outlines — the
+          neural network (left) does far worse than the symbolic regression
+          algorithm (right).
+          <video
+            src="https://d2r55xnwy6nx47.cloudfront.net/uploads/2022/05/solar_system_gnets.mp4"
+            autoPlay="autoplay"
+            muted="true"
+            loop="true"
+            playsinline="true"
+            width="100%"
+          ></video>
+          REF:
+          https://www.quantamagazine.org/machine-scientists-distill-the-laws-of-physics-from-raw-data-20220510/
         </p>
       </main>
     </>
